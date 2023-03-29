4601ca331e707f39fbf3f0b1d57f5118729890b1
Gabriel <100374129+projetosprimogabriel@users.noreply.github.com>
2023-03-29 14:32:45 -0300

    Add files via upload

---
+++ Exemplo/Produto.cs
@@ -0,0 +1,21 @@
+﻿namespace Exemplo;
+class Produto
+{
+    private string? Nome {get;set;}
+
+    private double Preco {get;set;}
+
+    public void MostraDados(){
+       Console.WriteLine("Nome: " + this.Nome);
+       Console.WriteLine("Preço: " + this.Preco);
+    }
+
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+
+    public void AlteraPreco(double preco){
+        this.Preco = preco;
+    }
+}
+
