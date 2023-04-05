532b2c119c0d29b640004a9aff4f87b7a501735f
Gabriel <100374129+projetosprimogabriel@users.noreply.github.com>
2023-04-05 14:21:51 -0300

    Add files via upload

---
+++ Exercicioalunos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicioalunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        alunos aluno1 = new alunos();
+        aluno1.nome = "fulano da silva";
+        aluno1.nota1 = 4.3;
+        aluno1.nota2 = 7.8;
+
+        aluno1.mensagem();
+    }
+}
