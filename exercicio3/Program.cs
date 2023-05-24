e01e5f15a4ed162c4df70ef3bee8c5acd3690ccf
Gabriel <100374129+projetosprimogabriel@users.noreply.github.com>
2023-05-24 14:43:05 -0300

    Add files via upload

---
+++ exercicio3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace exercicio3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContratoPessoaFisica contratoPessoaFisica = new ContratoPessoaFisica("Contrato Pessoa Física", 7000.0, 12, "321.564.879-00", 30);
+        contratoPessoaFisica.ExibirDetalhes();
+        Console.WriteLine("Valor da Prestação: " + contratoPessoaFisica.CalcularPrestacao());
+        Console.WriteLine();
+
+        ContratoPessoaJuridica contratoPessoaJuridica = new ContratoPessoaJuridica("Contrato Pessoa Jurídica", 3000.0, 24, "15.658.743/0009-00", "987456123");
+}
+}
